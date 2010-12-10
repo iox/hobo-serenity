3e81638d7680288b89b6ee35f4b27a52f493f6ee
Ignacio Huerta <ignacio@ihuerta.net>
2010-12-10 13:38:24 +0100

    small fix

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'serenity'
 ActiveRecord::Base.send(:include, Serenity::Generator)
 
 # Y ahora hobo_serenity.
-require 'hobo_serenity'
+require 'hobo-serenity'
 ActiveRecord::Base.send(:include, HoboSerenity)
 
 
