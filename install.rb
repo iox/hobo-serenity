a2e229dfb71a4c1d18542801dad6f3b799b5bf66
Ibon Castilla Varela <ibon.castilla@gmail.com>
2015-10-10 16:45:12 +0200

    LibreOffice 4+ incorporates some file format conversion tools.
    * Avoid DocumentConverter.py: valid for ancient OpenOffice and Python installations, but obsotele in new environments.
    * Filter programatically if you want to create PDF files or not, and if you want to keep ODT files or not.
    * Minor fixes (I18n, installation notes and so).

--- install.rb
+++ install.rb
@@ -5,5 +5,5 @@ unless File.exist?('app/views/odt_templates')
   FileUtils.mkdir('app/views/odt_templates')
 end
 
-#Copiar el ejemplo de plantilla
-FileUtils.copy('vendor/plugins/hobo-serenity/ejemplo.odt','app/views/plantillas_odt/ejemplo.odt')
+# Copy example template
+FileUtils.copy('vendor/plugins/hobo-serenity/example.odt','app/views/odt_templates/example.odt')
--- lib/hobo-serenity.rb
+++ lib/hobo-serenity.rb
@@ -6,101 +6,93 @@
 # This module will be added to the model
 module HoboSerenity
 
-    # We include this in order to use "number_to_currency" in the ODT template
-    include ActionView::Helpers::NumberHelper
+  # We include this in order to use "number_to_currency" in the ODT template
+  include ActionView::Helpers::NumberHelper
 
-    def create_document (document_path, template_name)
-      # "ODT_PATH" is the main folder where every document is saved
-      # "document_path" can have subfolders
-      # Here we prepare "document_folder" based on the two previous variables
-      document_folder = ODT_PATH
-      document_path_split = document_path.split('/')
-      document_path_split.each_with_index {|x, y| 
-        if (y != document_path_split.length-1)
-          document_folder += x + '/'
-        end
-      }
-      
-      # if "document_folder" doesn't exist, create it
-      unless File.exist?(document_folder)
-        FileUtils.mkdir_p(document_folder)
+  def create_document(document_path, template_name, delete_odt=false, create_pdf=true)
+    # "ODT_PATH" is the main folder where every document is saved
+    # "document_path" can have subfolders
+    # Here we prepare "document_folder" based on the two previous variables
+    document_folder = ODT_PATH
+    document_path_split = document_path.split('/')
+    document_path_split.each_with_index {|x, y| 
+      if (y != document_path_split.length-1)
+        document_folder += x + '/'
       end
-      
-      # Prepare the files paths
-      template = 'app/views/odt_templates/'+ template_name +'.odt'
-      odt_path = document_folder + document_path_split.last + '.odt'
-      pdf_path = document_folder + document_path_split.last + '.pdf'
+    }
 
-      # Generate ODT with Serenity
-      render_odt template, odt_path
-      
-      # Convert to PDF with OpenOffice 
-      # OpenOffice should be listening as Server in localhost:8100
-      command = 'python ' +
-          'vendor/plugins/hobo-serenity/lib/DocumentConverter.py ' +
-          odt_path + ' ' + pdf_path
-      system(command)
-      
-      # By default we delete the ODT
-      unless KEEP_ODT
-        File.delete(odt_path)
-      end
-      
+    # if "document_folder" doesn't exist, create it
+    unless File.exist?(document_folder)
+      FileUtils.mkdir_p(document_folder)
     end
+
+    # Prepare the files paths
+    template = 'app/views/odt_templates/'+ template_name +'.odt'
+    odt_path = document_folder + document_path_split.last + '.odt'
+    pdf_path = document_folder + document_path_split.last + '.pdf'
+
+    # Generate ODT with Serenity
+    render_odt template, odt_path
+    File.chmod(0664, odt_path)
+      
+    # Convert to PDF via LibreOffice
+
+    command = "soffice --headless --convert-to pdf --outdir '#{[Rails.root, document_folder].join('/')}' '#{[Rails.root, odt_path].join('/')}' &>>#{[Rails.root,'log',Rails.env + ".log"].join('/')}"
+    `command` # In order to avoid race conditions troubles, use backticks instead system.
+
+    # By default we delete the ODT
+    File.delete(odt_path) if delete_odt == true
+
+  end
     
-    
-    # These are the paths that the download link will use
-    def odt_download_path(document)
-      model = self.class.to_s.downcase
-      return "/"+ model +"s/download_odt/" + id.to_s +
-              '?h=' + rand.to_s + '&document=' + document
-    end
-    
-    def pdf_download_path(document)
-      model = self.class.to_s.downcase
-      return "/"+ model +"s/download_pdf/" + id.to_s + 
-              '?h=' + rand.to_s + '&document=' + document
-    end
+  # These are the paths that the download link will use
+  def odt_download_path(document)
+    model = self.class.to_s.downcase
+    return "/"+ model +"s/download_odt/" + id.to_s +
+            '?h=' + rand.to_s + '&document=' + document
+  end
+
+  def pdf_download_path(document)
+    model = self.class.to_s.downcase
+    return "/"+ model +"s/download_pdf/" + id.to_s + 
+           '?h=' + rand.to_s + '&document=' + document
+  end
 
 end
-
-
-
 
 # This additional module with be added to the controller to handle downloads
 module HoboSerenityController
-   def download_odt
-     # In order to create the document first we need to access the model object
-     controller_name = self.class.to_s.downcase 
-     model_name = controller_name.split('scontroller')[0].humanize
-     object = model_name.constantize.find(params[:id])
+  def download_odt
+    # In order to create the document first we need to access the model object
+    controller_name = self.class.to_s.downcase 
+    model_name = controller_name.split('scontroller')[0].humanize
+    object = model_name.constantize.find(params[:id])
      
-     # The template name is in params, and the document path is in the model
-     template_name = params[:document]
-     document_path = object.send(template_name)
+    # The template name is in params, and the document path is in the model
+    template_name = params[:document]
+    document_path = object.send(template_name)
      
-     # Create the document
-     object.create_document(document_path, template_name)     
-     
-     # Send the file to the user
-     send_file ODT_PATH + document_path + '.odt'
-   end 
-   
-   def download_pdf
-     # In order to create the document first we need to access the model object
-     controller_name = self.class.to_s.downcase 
-     model_name = controller_name.split('scontroller')[0].humanize
-     object = model_name.constantize.find(params[:id])
-     
-     # The template name is in params, and the document path is in the model
-     template_name = params[:document]
-     document_path = object.send(template_name)
-     
-     # Create the document
-     object.create_document(document_path, template_name)     
-     
-     # Send the file to the user
-     send_file ODT_PATH + document_path + '.pdf'
-   end 
-end
+    # Create the document
+    object.create_document(document_path, template_name)     
 
+    # Send the file to the user
+    send_file ODT_PATH + document_path + '.odt'
+  end 
+   
+  def download_pdf
+    # In order to create the document first we need to access the model object
+    controller_name = self.class.to_s.downcase 
+    model_name = controller_name.split('scontroller')[0].humanize
+    object = model_name.constantize.find(params[:id])
+     
+    # The template name is in params, and the document path is in the model
+    template_name = params[:document]
+    document_path = object.send(template_name)
+
+    # Create the document
+    object.create_document(document_path, template_name)     
+     
+    # Send the file to the user
+    send_file ODT_PATH + document_path + '.pdf'
+  end
+end
