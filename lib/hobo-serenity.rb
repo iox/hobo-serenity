cadbaffe3e182c2387c84d34c45545b7abc16fc5
Ibon Castilla Varela <ibon.castilla@gmail.com>
2016-01-18 19:55:51 +0100

    Apply LibreOffice 4 improvements to convert to PDF.

--- lib/hobo-serenity.rb
+++ lib/hobo-serenity.rb
@@ -1,7 +1,7 @@
 # HoboSerenity
 
 # For debugging: you need to call 'logger' like this before using it
-# logger = RAILS_DEFAULT_LOGGER
+#logger = RAILS_DEFAULT_LOGGER
 
 # This module will be added to the model
 module HoboSerenity
@@ -10,89 +10,82 @@ module HoboSerenity
   include ActionView::Helpers::NumberHelper
 
   def create_document(document_path, template_name, delete_odt=false, create_pdf=true)
-    # "ODT_PATH" is the main folder where every document is saved
-    # "document_path" can have subfolders
-    # Here we prepare "document_folder" based on the two previous variables
-    document_folder = ODT_PATH
-    document_path_split = document_path.split('/')
-    document_path_split.each_with_index {|x, y| 
-      if (y != document_path_split.length-1)
-        document_folder += x + '/'
-      end
-    }
+    # document_path: where do you want to store redered template once parsed.
+    # template_name: which template do you want to be rendered.
 
+    # Here we prepare "document_folder" based on the two previous variables
+    document_folder = document_path.split('/')[0...-1].join('/')
+      
     # if "document_folder" doesn't exist, create it
     unless File.exist?(document_folder)
       FileUtils.mkdir_p(document_folder)
     end
 
     # Prepare the files paths
-    template = 'app/views/odt_templates/'+ template_name +'.odt'
-    odt_path = document_folder + document_path_split.last + '.odt'
-    pdf_path = document_folder + document_path_split.last + '.pdf'
+    template = ODT_TEMPLATES_PATH + template_name +'.odt'
+    odt_path = document_path + '.odt'
+    pdf_path = document_path + '.pdf'
 
     # Generate ODT with Serenity
     render_odt template, odt_path
     File.chmod(0664, odt_path)
       
-    # Convert to PDF via LibreOffice
-
-    command = "soffice --headless --convert-to pdf --outdir '#{[Rails.root, document_folder].join('/')}' '#{[Rails.root, odt_path].join('/')}' &>>#{[Rails.root,'log',Rails.env + ".log"].join('/')}"
-    `command` # In order to avoid race conditions troubles, use backticks instead system.
+    # Convert to PDF via LibreOffice 
+    if create_pdf == true
+      odt_to_pdf_cmd = "soffice --headless --convert-to pdf --outdir '#{[Rails.root, document_folder].join('/')}' '#{[Rails.root, odt_path].join('/')}' &>>#{[Rails.root,'log',Rails.env + ".log"].join('/')}"
+      `#{odt_to_pdf_cmd}` # In order to avoid race condition trouble, use backticks instead system.
+    end
 
     # By default we delete the ODT
     File.delete(odt_path) if delete_odt == true
-
   end
     
   # These are the paths that the download link will use
-  def odt_download_path(document)
-    model = self.class.to_s.downcase
-    return "/"+ model +"s/download_odt/" + id.to_s +
-            '?h=' + rand.to_s + '&document=' + document
+  def odt_download_url(document)
+    return ["/",self.class.to_s.downcase.pluralize,"/download_odt/",id.to_s,'?h=',rand.to_s,'&document=',document].join
   end
 
-  def pdf_download_path(document)
-    model = self.class.to_s.downcase
-    return "/"+ model +"s/download_pdf/" + id.to_s + 
-           '?h=' + rand.to_s + '&document=' + document
+  def pdf_download_url(document)
+    return ["/",self.class.to_s.downcase.pluralize,"/download_pdf/",id.to_s,'?h=',rand.to_s,'&document=',document].join
   end
 
 end
 
-# This additional module with be added to the controller to handle downloads
+# This additional module will be added to the controller to handle downloads
 module HoboSerenityController
+
   def download_odt
     # In order to create the document first we need to access the model object
     controller_name = self.class.to_s.downcase 
     model_name = controller_name.split('scontroller')[0].humanize
     object = model_name.constantize.find(params[:id])
-     
+
     # The template name is in params, and the document path is in the model
     template_name = params[:document]
     document_path = object.send(template_name)
-     
+
     # Create the document
-    object.create_document(document_path, template_name)     
+    object.create_document(document_path, template_name)
 
     # Send the file to the user
-    send_file ODT_PATH + document_path + '.odt'
+    send_file document_path + '.odt', :type =>  "application/vnd.oasis.opendocument.text"
   end 
-   
+
   def download_pdf
     # In order to create the document first we need to access the model object
     controller_name = self.class.to_s.downcase 
     model_name = controller_name.split('scontroller')[0].humanize
     object = model_name.constantize.find(params[:id])
-     
+
     # The template name is in params, and the document path is in the model
     template_name = params[:document]
     document_path = object.send(template_name)
 
     # Create the document
-    object.create_document(document_path, template_name)     
-     
+    object.create_document(document_path, template_name)
+
     # Send the file to the user
-    send_file ODT_PATH + document_path + '.pdf'
-  end
+    send_file document_path + '.pdf', :type =>  "application/pdf"
+  end 
+
 end
